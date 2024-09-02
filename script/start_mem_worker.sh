c02089de8d3638e26a13737df50bbb9378c4a778
jerrychen1990 <jerrychen1990@gmail.com>
2024-09-02 20:14:15 +0800

    添加redis检测

--- script/start_mem_worker.sh
+++ script/start_mem_worker.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+# 检查 Redis 是否在运行
+if pgrep redis-server > /dev/null
+then
+    echo "Redis is already running."
+else
+    echo "Redis is not running. Starting Redis..."
+    redis-server &
+fi
+# 开启消息处理队列
+echo "Starting Celery worker to handle messages..."
 celery -A aifori.task worker --loglevel=info
